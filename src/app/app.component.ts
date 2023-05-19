13262606e9918c5d1725ab95bdf2d3af6b113cd0
Leon B <leon.borlinghaus@gmail.com>
2023-05-19 14:51:17 +0200

    add stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
+import { take, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  private promptEvent: any;
+
   title = 'blocus-game';
+
+  constructor(private platform: Platform) {
+    this.initPwaPrompt();
+  }
+
+  public initPwaPrompt() {
+    if (this.platform.ANDROID) {
+      window.addEventListener('beforeinstallprompt', (event: any) => {
+        event.preventDefault();
+        this.promptEvent = event;
+        this.openPromptComponent('android');
+      });
+    }
+    if (this.platform.IOS) {
+      const isInStandaloneMode =
+        'standalone' in window.navigator && window.navigator['standalone'];
+      if (!isInStandaloneMode) {
+        this.openPromptComponent('ios');
+      }
+    }
+  }
+
+  private openPromptComponent(mobileType: 'ios' | 'android') {
+    timer(3000)
+      .pipe(take(1))
+      .subscribe(() => {
+        console.log('open modal');
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,7 @@ import { GamepadComponent } from './components/gamepad/gamepad.component';
 import { IndexComponent } from './views/index.component';
 import { TokenComponent } from './components/token/token.component';
 import { StateService } from './services/state.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,12 @@ import { StateService } from './services/state.service';
     IndexComponent,
     TokenComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [StateService],
   bootstrap: [AppComponent],
 })
